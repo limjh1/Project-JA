7bc0684663fc2215aba6736d484750e44d218058
jhlim <jehyuk1711@gmail.com>
2025-11-10 22:14:56 +0900

    Input Data Asset, Input Component

---
+++ Source/JA/Private/Components/Input/JAInputComponent.cpp
@@ -0,0 +1,5 @@
+// JhLim All Rights Reserved
+
+
+#include "Components/Input/JAInputComponent.h"
+
---
+++ Source/JA/Public/Components/Input/JAInputComponent.h
@@ -0,0 +1,33 @@
+// JhLim All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "DataAssets/Input/DataAsset_InputConfig.h"
+#include "JAInputComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JA_API UJAInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+
+public:
+	template<class UserObject, typename CallbackFunc>
+	void BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func);
+	
+};
+
+template<class UserObject, typename CallbackFunc>
+inline void UJAInputComponent::BindNativeInputAction(const UDataAsset_InputConfig* InInputConfig, const FGameplayTag& InInputTag, ETriggerEvent TriggerEvent, UserObject* ContextObject, CallbackFunc Func)
+{
+	checkf(InInputConfig, TEXT("Input Config Data Asset is null, can not proceed with binding"));
+
+	if (UInputAction* FoundAction = InInputConfig->FindNativeInputActionByTag(InInputTag))
+	{
+		BindAction(FoundAction, TriggerEvent, ContextObject, Func);
+	}
+}
